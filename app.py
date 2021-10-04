004f2a0e45b90f7b4b7eba4c485fb3f12c938ae9
Filippo Utro <futro@us.ibm.com>
2021-10-04 19:28:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+
+import logging
+import logstash
+import sys
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -13,5 +18,32 @@ def get_version():
 def get_test():
     return '<h1>You are accessing /test endpoint</h1>'
 
+
+
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+    host = 'localhost'
+
+    test_logger = logging.getLogger('python-logstash-logger')
+    test_logger.setLevel(logging.INFO)
+    test_logger.addHandler(logstash.LogstashHandler(host, 5959, version=1))
+# test_logger.addHandler(logstash.TCPLogstashHandler(host, 5959, version=1))
+
+    test_logger.error('python-logstash: test logstash error message.')
+    test_logger.info('python-logstash: test logstash info message.')
+    test_logger.warning('python-logstash: test logstash warning message.')
+
+# add extra field to logstash message
+    extra = {
+       'test_string': 'python version: ' + repr(sys.version_info),
+       'test_boolean': True,
+       'test_dict': {'a': 1, 'b': 'c'},
+       'test_float': 1.23,
+       'test_integer': 123,
+       'test_list': [1, 2, '3'],
+    }
+    test_logger.info('python-logstash: test extra fields', extra=extra)
+   
